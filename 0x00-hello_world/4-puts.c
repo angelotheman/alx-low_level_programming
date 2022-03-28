793ac8ea7a7435d5e2aaf4104072c5c70cd4a604
Kwabena Twumasi <angelotheman@pop-os.localdomain>
2022-03-28 16:01:45 +0000

    Another fix to using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
