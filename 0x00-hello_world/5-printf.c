dd3a7576761bb20a637d6e0fd45e097d962ecc58
Kwabena Twumasi <angelotheman@pop-os.localdomain>
2022-03-28 16:15:05 +0000

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Prints quote
  *
- * Return - zero on success
+ * Return: zero on success
  *
  */
-int main()
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return 0;
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
