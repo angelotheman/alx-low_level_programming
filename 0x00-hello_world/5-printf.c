9b8b0bae0ede949e5d5ae2e4c29f1d198342f092
Angel Twumasi <ktwum721@gmail.com>
2023-07-13 16:34:56 +0000

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point of the code, this function only prints the sentence
+  * main - Prints "with proper grammar, but the outcome is
+  * a piece of art," followed by a newline.
   *
   * Return : Always 0 (Success)
   */
