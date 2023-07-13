ee9b3f7e252acfac7eddfa9bc69bf9757bf8e7e3
Angel Twumasi <ktwum721@gmail.com>
2023-07-13 15:55:34 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
   *
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
