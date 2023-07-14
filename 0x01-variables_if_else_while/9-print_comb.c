a6af723b37006fa205f300d37a5a2d1e7e87e536
Angel Twumasi <ktwum721@gmail.com>
2023-07-14 14:53:56 +0000

    Combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,15 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
 
 	return (0);
 }
