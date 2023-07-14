9fdb183b2ff952bf22f5fe5b9e16581b112d0a91
Kwabena Twumasi <61213902+angelotheman@users.noreply.github.com>
2023-07-14 05:35:17 +0000

    Hello, Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints "with proper grammar, but the outcome is
-  * a piece of art," followed by a newline.
+  * main - Entry point for the function
   *
   * Return : Always 0 (Success)
   */
