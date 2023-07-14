95169f6a3051fa4f3555b37de1d9dac43c351a31
Angel Twumasi <ktwum721@gmail.com>
2023-07-14 13:26:40 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+i#include <stdio.h>
+
+/**
+  * main - Entry point for the code
+  *
+  * Return: 0 since main is void
+  */
+
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
