b41281d68ab37e10a5ebe337b307ccc2cd39dc18
Angel Twumasi <ktwum721@gmail.com>
2023-07-14 12:33:26 +0000

    Using putchar instead of puts and printf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of code
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 123; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
