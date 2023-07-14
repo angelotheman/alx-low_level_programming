461c8e05570b3b0ee99814bae3ec855a0cb61f22
Angel Twumasi <ktwum721@gmail.com>
2023-07-14 15:13:07 +0000

    Digit combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,18 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
-	putchar(9 + '0');
+	putchar('\n');
 
 	return (0);
 }
