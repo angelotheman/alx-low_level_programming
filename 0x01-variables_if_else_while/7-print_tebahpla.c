a6356df97dd8dbca5ec0bccdf60aad290b56cbc7
Angel Twumasi <ktwum721@gmail.com>
2023-07-14 13:28:00 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point for the code
+  *
+  * Return: 0 since main is void
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
