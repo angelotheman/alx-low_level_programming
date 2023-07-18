a3506d1c5786f9a49678971881bcee44721826ac
Angel Twumasi <ktwum721@gmail.com>
2023-07-18 17:52:44 +0000

    Calculate absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _abs - This function calculates the absolute value of digits
+  * @int: Value to be processed
+  *
+  * Returns: The absolute number of the integer
+  */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int value = 0 - int;
+		return (int);
+	}
+	else
+	{
+		return (int);
+	}
+}
