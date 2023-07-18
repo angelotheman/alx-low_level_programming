422cc53165b99ee831b9ded83df99b8d5d1fd076
Angel Twumasi <ktwum721@gmail.com>
2023-07-18 14:03:30 +0000

    Wonderful beginnings

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 
 /**
   * main - Entry point for the code
@@ -8,7 +8,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
-#include <stdio.h>
+/**
+  * Description: These header files provide the functionalities needed
+  */
 
+
+int _putchar(char c)
 void print_alphabet(void);
