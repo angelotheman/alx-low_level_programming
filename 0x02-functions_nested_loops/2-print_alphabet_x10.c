b41f40d1fdcf3036644676a6db41b48a5923c3bb
Angel Twumasi <ktwum721@gmail.com>
2023-07-18 14:10:44 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10(void) - Prints the alphabets 10 times
+  *
+  * Return: 0 (Success)
+  */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
