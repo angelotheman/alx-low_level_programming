6131379490112da25bfb43c17af6a6348c17c078
Angel Twumasi <ktwum721@gmail.com>
2023-07-18 18:53:37 +0000

    Calculate the absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-  * _abs - This function calculates the absolute value of digits
-  * @a: Value to be processed
+  * _abs - This function calculates the absolute value of digits.
+  * @a: Value to be processed.
   *
-  * Returns: The absolute number of the integer
+  * Return: The absolute number of the integer.
   */
 
 int _abs(int a)
 {
 	if (a < 0)
 	{
-		int value = 0 - a;
-		return (value);
+		return (-a);
 	}
 	else
 	{
