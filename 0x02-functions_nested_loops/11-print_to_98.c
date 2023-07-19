528914bb8418d827b4304161e4dd49b403c60a29
Angel Twumasi <ktwum721@gmail.com>
2023-07-19 00:31:05 +0000

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,22 +10,22 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n < 98)
+	{
+		for (n =n ; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
 	{
 		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
 	}
-	else if (n == 98)
+	else 
 	{
-		printf("%d\n", n);
-	}
-	else
-	{
-		for (n = n; n < 98; n++)
-		{
-			printf("%d, ",n);
-		}
+		printf("98\n");
 	}
 }
