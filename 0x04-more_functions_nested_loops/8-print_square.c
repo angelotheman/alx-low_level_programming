34375a5f383889a61240e01bfd0bb78b18a3e722
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 18:45:08 +0000

    You are so much sunshine in every square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_square - Printing a square of all numbers
+  * @size: The size of the square to be printed
+  *
+  * Return: A square printed with #
+  */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
