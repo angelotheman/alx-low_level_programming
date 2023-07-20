41e0661c84762a873a75b6011081fee28953d613
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 15:40:11 +0000

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * more_numbers - Printing more numbers of the given set
+  */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
