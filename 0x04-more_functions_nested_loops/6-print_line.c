9c08f1a6c5cac5be1b9596fc92ac5056a1771c64
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 23:10:37 +0000

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,13 @@ void print_line(int n)
 	int a;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
-
-	for (a = 0; a < n; a++)
-		_putchar('_');
-	_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
