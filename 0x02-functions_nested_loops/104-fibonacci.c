7f31b944c3368c35e92b1304776ba4eabbaa4e6f
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 01:56:35 +0000

    Highest Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Prints out fibonacci sequence
+  *
+  * Return: 0 - I am a shark boy
+  */
+
+int main(void)
+{
+	int i;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+
+	for (i = a; i <= 98; i++)
+	{
+		unsigned long int temp = a + b;
+
+		printf("%lu", temp);
+
+		a = b;
+		b = temp;
+
+		if (i < 98)
+			printf("\n");
+	}
+}
