df370792551fb2d718ac163adb5b7fffcb6f5603
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 18:33:19 +0000

    Print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Print diagonal characters
+  * @n: The number of times to be printed
+  *
+  * Return: 0 Success
+  */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+	int sum = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < sum; b++)
+		{
+			_putchar(' ');
+		}
+		sum++;
+		_putchar('\n');
+	}
+
+	return (0);
+}
