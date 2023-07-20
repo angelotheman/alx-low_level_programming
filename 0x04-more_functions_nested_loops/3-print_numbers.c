66476929fa530334675bcc5f686dc79e1d72162a
Angel Twumasi <ktwum721@gmail.com>
2023-07-20 15:08:32 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints all numbers in a range
+  *
+  * Return: 0 (Success)
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
