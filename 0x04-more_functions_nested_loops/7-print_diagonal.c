5cfc75e10b3d3a567c8d216e65ab278156cea8f0
Angel Twumasi <ktwum721@gmail.com>
2023-07-21 00:27:29 +0000

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,21 @@ void print_diagonal(int n)
 {
 	int a;
 	int b;
-	int sum = 0;
 
 	if (n <= 0)
-		_putchar('\n');
-
-	for (a = 0; a < n; a++)
 	{
-		for (b = 0; b < sum; b++)
-		{
-			_putchar(' ');
-		}
-		sum++;
 		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
