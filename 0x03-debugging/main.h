6a34f757c78920c2ea3a7f8ad6aa71d52303777a
Angel Twumasi <ktwum721@gmail.com>
2023-07-21 02:13:55 +0000

    Initial commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+  * Description: Functions for debugging
+  */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day)
