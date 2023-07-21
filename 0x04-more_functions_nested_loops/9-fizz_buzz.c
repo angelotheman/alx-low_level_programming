4041a9fce0aab14c7f7ff9adcb6330dbd0546299
Angel Twumasi <ktwum721@gmail.com>
2023-07-21 00:38:48 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - Printing Fizz buzz
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+
+		if (a != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
