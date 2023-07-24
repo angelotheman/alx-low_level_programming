42f1b2c5da0966a6c822bf3afbbee60acef42167
Angel Twumasi <ktwum721@gmail.com>
2023-07-24 21:28:38 +0000

    Printing half of sentences

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * puts_half - Printing half of a string
+  * @str: String to be printed
+  *
+  */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int startIdx = length / 2;
+
+	if (length % 2 != 0)
+		startIdx = (length - 1) / 2;
+
+	char *second_half = str + startIdx;
+
+	while (*second_half != '\0')
+	{
+		_putchar(*second_half);
+		second_half++;
+	}
+}
