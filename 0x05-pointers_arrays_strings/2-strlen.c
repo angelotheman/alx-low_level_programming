f5afb1a18754498c2768029b1aecf72a931695d0
Angel Twumasi <ktwum721@gmail.com>
2023-07-24 14:48:03 +0000

    Finding the length of the string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,30 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
   * _strlen - Returns the length of a string
   * @s: The string input
   *
-  * Returns: Length of string (int)
+  * Return: Length of string (int)
   */
 
 int _strlen(char *s)
 {
 	int count = 0;
-	int i;
 
-	for (i = *s; i <= '\0'; i++)
+	while (*s != '\0')
 	{
 		count++;
+		s++;
 	}
+
 	return (count);
-}
-
-int main(void)
-{
-	char a[] = "School";
-
-	printf("The length is: %d\n", _strlen(&a));
-	
-	return (0);
 }
