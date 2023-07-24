eff0de588f30c728a5bf7e760c403d101a050d80
Angel Twumasi <ktwum721@gmail.com>
2023-07-24 20:59:59 +0000

    Printing new lines at two intervals

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * puts2 - Print every other character of a string
+  * @str: The string to be printed
+  *
+  */
+
+void puts2(char *str)
+{
+	if (str == NULL)
+		return;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+
+		str += 2;
+	}
+	_putchar('\n');
+}
