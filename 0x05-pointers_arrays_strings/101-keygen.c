1e56a81817bb20a21388fad0a471a617bb9af54d
Kwabena Twumasi <ktwum721@gmail.com>
2023-07-25 18:01:55 +0000

    Password Generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+  * main - generates a random valid password
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		_putchar(pass[i] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			_putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
