9e336218263ae3c4cb576d26b5ed606c1e17259d
Kwabena Twumasi <ktwum721@gmail.com>
2023-07-28 00:46:02 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	for (a = 0; a < n && src[a] != '\0'; a++)
-		dest[a] = src[a];
+	a = 0;
 
-	for ( ; a < n; a++)
-		dest[i] = '\0';
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
 
 	return (dest);
 }
