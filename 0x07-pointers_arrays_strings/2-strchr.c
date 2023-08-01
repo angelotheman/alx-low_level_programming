8b0711a54965bdb668e5a47f94902f25b885dd5c
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-01 00:44:03 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strchr - Locates a character in a string
+  * @s: String to be searched
+  * @c: Character to be found
+  *
+  * Return: pointer to first occurrence
+  *         or NULL if not found
+  */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
