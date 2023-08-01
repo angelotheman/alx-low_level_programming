7f803bd5598bebcde1fe8799d2b6fe8537cb6591
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-01 00:16:32 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (i = 0; i < n && s[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-
-	s[i] = '\0';
-	
 	return s;
 }
