b2d576ced91309e6f792830db4a20caffc43bda8
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-02 11:26:42 +0000

    Factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * factorial - Factorial of a given number
+  * @n: Given number
+  *
+  * Return: Int (Success)
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
