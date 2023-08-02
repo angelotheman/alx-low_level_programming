816d42090dc51b56e81f744a565a7d31591459a9
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-02 10:36:24 +0000

    Puts Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - Prints a string
+  * @s: String to be printed
+  *
+  * Return: 0 (Success)
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s++);
+}
