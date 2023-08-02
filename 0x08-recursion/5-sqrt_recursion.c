5364d39ac5b73ab0105646d1ad71bc1ddcab2b13
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-02 18:44:28 +0000

    Square Root Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,21 +3,53 @@
 /**
   * _sqrt_recursion - Returns natural square of number
   * @n: Argument passed
+  * @low: Args
+  * @high: Args
   *
   * Return: Square of number.
   * Example: int _sqrt_recursion(int 4)
   *	     Return 2
   */
 
+int sqrt_binary_search(int n, int low, int high);
+
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
+	int (n <= 0)
+		return (-1);
 
-	int root = _sqrt_recursion(n / 2);
+	return (sqrt_binary_search(n, 0, n));
+}
 
-	if (root * root == n)
-		return root;
-	else
-		return (root + 1);
+/**
+  * sqrt_binary_search - Returns the value to determine sqrt
+  * @n: first argument
+  * @low: second argument
+  * @high: last argument
+  *
+  * Return: The floor of the squre root
+  */
+
+int sqrt_binary_search(int n, int low, int high)
+{
+	if (low <= high)
+	{
+		int mid = (low + high) / 2;
+		int square = mid * mid;
+
+		if (square == n)
+		{
+			return (mid);
+		}
+		else if (square > n)
+		{
+			return (sqrt_binary_search(n, low, mid - 1));
+		}
+		else
+		{
+			return (sqrt_binary_search(n, mid + 1, high));
+		}
+	}
+
+	return (high);
 }
