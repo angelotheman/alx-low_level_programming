e238073ad7fb713580576749fe3d8b8365cf1fae
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-03 22:53:55 +0000

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of program
+  * @argc - First argument
+  * @argv - Second argument
+  *
+  * Return: 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
