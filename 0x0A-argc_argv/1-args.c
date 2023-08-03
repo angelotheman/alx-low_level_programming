8b5c1dc37b8b9e36edeff7297c61319710eae703
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-03 23:28:03 +0000

    Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  * @argc: First argument
+  * @argv: Second argument
+  *
+  * Return: 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
