512ba906294ab58931788d00e69045f878887c0f
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-03 00:51:10 +0000

    Wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,22 +4,48 @@
   * wildcmp - Checks for identical strings
   * @s1: First string
   * @s2: Second string
+  * @s: String for next function
   *
   * Return: 1 if equal, 0 otherwise
   */
 
+int is_end_of_string(char *s);
+
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
+	if (is_end_of_string(s2))
 	{
-		return (*s1 == '\0');
+		return (is_end_of_string(s1));
 	}
 	else if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		if (is_end_of_string(s2 + 1))
+		{
+			return (1);
+		}
+		else if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		}
 	}
 	else
 	{
-		return (*s1 == *s2 && wildcmp(s1 + 1, s2 + 1));
+		return ((*s1 == *s2) && wildcmp(s1 + 1, s2 + 1));
 	}
+}
+
+/**
+  * is_end_of_string - Check if it's a string
+  * @s: String arg
+  *
+  * Return: 1 if string, 0 otherwise
+  */
+
+int is_end_of_string(char *s)
+{
+	return (*s == '\0');
 }
