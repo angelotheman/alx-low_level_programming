4d795739b8f505a5ffcede4c4a5ba675468064c3
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-07 21:19:55 +0000

    Created array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * create_array - Create array of chars
+  * @size: First argument
+  * @c: Second argument
+  *
+  * Return: NULL if size is 0, pointer to array
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *ptr = (char *)malloc(size * sizeof(c));
+
+	return (ptr);
+}
