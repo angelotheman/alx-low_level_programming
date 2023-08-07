22b40b9a327c5ecbd05df4ea461060dcc58394e2
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-07 22:38:54 +0000

    Code the strdup function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,13 +11,19 @@
 char *_strdup(char *str)
 {
 	char *new_string;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	new_string = (char *)malloc(strlen(str) + 1);
+	new_string = (char *)malloc(i);
 
 	if (new_string == NULL)
 	{
