f9deee87d77571fdd410d64499f6bfc5f2f21721
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-07 21:50:52 +0000

    Create array and initialize with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,11 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(size * sizeof(c));
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c;
