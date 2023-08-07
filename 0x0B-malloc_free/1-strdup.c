a000ec81f6a6920357fd778df75fd3936adacc28
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-07 22:26:19 +0000

    Code the strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - Pointer to newly allocated space
+  * @str: The string argument
+  *
+  * Return: NULL for insufficient memory, pointer otherwise
+  */
+
+char *_strdup(char *str)
+{
+	char *new_string;
+
+	if (*str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_string = (char *)malloc(3 * sizeof(str));
+
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*str != '\0')
+	{
+		*new_string = *str;
+		new_string++;
+		str++;
+	}
+
+	new_string = '\0';
+
+	return (new_string);
+}
