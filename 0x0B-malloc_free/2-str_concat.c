c621b55666a8c86214e334893dc261d1470f7a79
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-08 16:12:27 +0000

    String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
 
+
+/**
+  * copy_string - Copies string from source to dest
+  * @src: source string
+  * @dest: destination string
+  *
+  * Return: 0 Success
+  */
+
+
+void copy_string(char *src, char *dest);
+
 /**
   * str_concat - Concatenate two strings with malloc
-  * copy_string - Helper function
   * @s1: First string
   * @s2: Second string
-  * @src: Source string
-  * @dest: Destination string
   *
   * Return: Pointer to string location
   */
 
-void copy_string(char *src, char *dest);
 
 char *str_concat(char *s1, char *s2)
 {
 	int s1_length = 0;
 	int s2_length = 0;
 	char *new_string;
-	int total_length = 0;
 
 	if (s1 == NULL || s2 == NULL)
 	{
@@ -36,9 +43,7 @@ char *str_concat(char *s1, char *s2)
 		s2_length++;
 	}
 
-	total_length = s1_length + s2_length;
-
-	new_string = (char *)malloc(total_length + 1);
+	new_string = (char *)malloc(s1_length + s2_length + 1);
 
 	if (new_string == NULL)
 	{
@@ -48,7 +53,7 @@ char *str_concat(char *s1, char *s2)
 	copy_string(s1, new_string);
 	copy_string(s2, new_string + s1_length);
 
-	new_string[total_length] = '\0';
+	new_string[s1_length + s2_length] = '\0';
 
 	return (new_string);
 }
@@ -60,6 +65,7 @@ char *str_concat(char *s1, char *s2)
   *
   * Return: 0 Success
   */
+
 
 void copy_string(char *src, char *dest)
 {
