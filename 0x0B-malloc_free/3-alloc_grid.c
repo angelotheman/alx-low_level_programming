48016f40845c032bb22e8d20f7678da3d984acc3
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-08 21:13:33 +0000

    Allocate memory for array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,10 +29,17 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < width; i++)
 	{
 		array[i] = (int *)malloc(height * sizeof(int *));
-	}
 
-	for (i = 0; i < width; i++)
-	{
+		if (array[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
+			return (NULL);
+		}
+
 		for (j = 0; j < height; j++)
 		{
 			array[i][j] = 0;
