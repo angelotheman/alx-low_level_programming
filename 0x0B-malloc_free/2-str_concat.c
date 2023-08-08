e25834021434d9e56985c667706cb84f50e0581d
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-08 19:57:58 +0000

    String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,10 +28,10 @@ char *str_concat(char *s1, char *s2)
 	int s2_length = 0;
 	char *new_string;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[s1_length] != '\0')
 	{
