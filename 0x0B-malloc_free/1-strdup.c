b02423216864389050a219915c0737a547622b3e
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-08 14:29:52 +0000

    Configured the strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,32 +11,32 @@
 char *_strdup(char *str)
 {
 	char *new_string;
-	int i = 0;
+	int length = 0;
 	int a;
-
-	while (str[i] != '\0')
-	{
-		i++;
-	}
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	new_string = (char *)malloc(i + 1);
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new_string = (char *)malloc(length + 1);
 
 	if (new_string == NULL)
 	{
 		return (NULL);
 	}
 
-	for (a = 0; a < i; a++)
+	for (a = 0; a < length; a++)
 	{
 		new_string[a] = str[a];
 	}
 
-	new_string[i] = '\0';
+	new_string[length] = '\0';
 
 	return (new_string);
 }
