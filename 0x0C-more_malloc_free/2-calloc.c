b563cde6fff1b4c3027fc8be04b33412028f8c71
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-10 23:26:15 +0000

    Calloc function coded

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = (int *)malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
