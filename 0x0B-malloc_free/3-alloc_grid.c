f6d72998f9de54870c54d7828d56f55439f4fdc0
Kwabena Twumasi <61213902+angelotheman@users.noreply.github.com>
2023-08-11 16:55:15 +0000

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int *)malloc(width * sizeof(int *));
+		array[i] = (int *)malloc(width * sizeof(int));
 
 		if (array[i] == NULL)
 		{
