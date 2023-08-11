feb35aca2a786596e924b89d548a243af07078be
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-11 00:37:27 +0000

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
 
 	if (new_size < old_size)
@@ -38,7 +38,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	memcpy(new_ptr, ptr, size);
-	
 	free(ptr);
 
 	return (new_ptr);
