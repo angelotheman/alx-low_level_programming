1e1982f4a249221b0f915d5a45565f2b45b36517
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-11 00:39:42 +0000

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
   * _realloc - Reallocates a memory block
@@ -7,6 +8,7 @@
   * @old_size: Old size of malloc
   * @new_size: New size of malloc
   *
+  * Return: Void pointer to new size
   */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
