9fe2edbe3e9bbf16d362dff9f1706b0d633fb497
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 18:38:01 +0000

    File output found

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the code
+  * @argc: Argument count
+  * @argv: Argument value
+  *
+  * Return: 0 Success
+  */
+
+int main(int argc, **char argv[])
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
