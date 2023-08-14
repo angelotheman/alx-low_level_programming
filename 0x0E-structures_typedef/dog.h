04a72f15122eb7f316268f09e9823caccf449fec
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 20:34:23 +0000

    Dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - Giving the properties of a dog
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  *
+  * Description: This struct gives attributes of a dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
