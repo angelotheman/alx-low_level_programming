402529228a0d21b590c67dc6c78cdd66579494e3
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 22:22:03 +0000

    Printing a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,8 @@
 
 void print_dog(struct dog *d)
 {
-	d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
-		return;
+		d = malloc(sizeof(struct dog));
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %lf\n", d->age ? d->age : "(nil)");
