c2b48c390b6657f6aafddd2b5aefa0c9131f45cd
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 21:10:29 +0000

    Initialize the dog struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,11 +14,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-
-	if (d == NULL)
-		exit(1);
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
