8be794dfe4720f1e9d251143fe3275ff53a3d1e1
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 21:25:40 +0000

    Initialized dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d)
 	{
-		d = malloc(sizeof(struct dog));
+		(*d).name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }
