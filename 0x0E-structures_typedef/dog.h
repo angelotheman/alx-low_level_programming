c001ff8c358a3b3e4af8df9d6ed0070aaee8dad8
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 21:00:12 +0000

    Updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,13 @@ struct dog
 	char *owner;
 };
 
+/**
+  * dog_t - Typedef for struct dog
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
