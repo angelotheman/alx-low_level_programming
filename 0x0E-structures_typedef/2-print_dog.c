97d0c87a3a73b3e20c306a67d664ee0c2b610af3
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-14 22:38:29 +0000

    Print dogs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,10 +18,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age == NULL)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %d\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
