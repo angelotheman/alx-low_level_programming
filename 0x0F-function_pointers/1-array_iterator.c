281b1b2b6a2e5e1b4875075db34214bcc90cc759
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-17 22:08:17 +0000

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - Executes a function
+  * @array: Args
+  * @size: Args
+  * @action: Function args
+  *
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!(array) || !(size) || !(action))
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
