3b317236d8d20170e8196d53d0572eb6e5663b97
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-18 02:18:42 +0000

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (count < n - 1 && separator != NULL)
 		{
-			printf("%c ", *separator);
+			printf("%s ", *separator);
 		}
 	}
 
