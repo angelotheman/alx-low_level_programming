056d3307de4de33f0caf3d16c315b9d663a7dd08
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-18 02:10:48 +0000

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - Function to print all numbers
+  * @separator: First argument
+  * @n: last args
+  *
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int count;
+
+	va_start(args, n);
+
+	for (count = 0; count < n; count++)
+	{
+		int x;
+
+		x = va_arg(args, int);
+		printf("%d", x);
+
+		if (count < n - 1)
+		{
+			if (!separator)
+				printf("%c ", *separator);
+		}
+	}
+
+	printf("\n");
+}
