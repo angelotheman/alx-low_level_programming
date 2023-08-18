902c451e46cfc96cceccf7587ee57f3f1fb56214
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-18 00:31:28 +0000

    One heavy task....sigh

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+  * get_op_func - Selects the correct function to perform
+  * @s: Arg
+  *
+  * Return: Pointer to the function
+  */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_div},
+		{NULL, NULL}
+	};
+
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops.[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+  * main - Main function to execute code
+  * @argc: Number of arguments
+  * @argv: Value of argument
+  *
+  * Return: 0 Success
+  */
+
+int main(int argc, char **argv[])
+{
+	int a, b, total;
+	char *op;
+	int (*ptr_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op = arg[2];
+
+	ptr_func = get_op_func(op);
+
+	if (ptr_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", ptr_func(a, b));
+
+	return (0);
+}
