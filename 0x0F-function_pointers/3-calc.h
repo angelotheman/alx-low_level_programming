e986ae46c38d4c74f880d78316b8dc11f7ea7b9d
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-18 00:33:36 +0000

    One heavy task....sigh

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
+#include <stdio.h>
 
 /**
   * struct op - Struct of operations
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
   * get_op_func - Selects the correct function to perform
