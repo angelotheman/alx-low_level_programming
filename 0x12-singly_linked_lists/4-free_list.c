5ad0cb13002ee6ce5da551370fe9d8e79c04e3fb
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-24 20:41:56 +0000

    Deallocate the linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * free_list - Deallocate memory allocated
+  * @head: Linked list
+  *
+  */
+
+void free_list(list_t *head)
+{
+	list_t *curr;
+	list_t *aux_node;
+
+	curr = head;
+
+	while (curr != NULL)
+	{
+		aux_node = curr;
+		curr = curr->next;
+		free(aux_node);
+	}
+
+	head == NULL;
+}
