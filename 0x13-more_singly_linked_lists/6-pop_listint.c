839a76dc043fe08e1f11c568bc840aaac4a0eb7a
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-29 21:33:27 +0000

    Pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * pop_listint - Deletes the head node and return it's data
+  * @head: Head node
+  *
+  * Return: 0 if empty or otherwise
+  */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *curr;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	data = (*head)->n;
+	curr = *head;
+	*head = (*head)->next;
+	
+	free(curr);
+
+	return (data);
+}
