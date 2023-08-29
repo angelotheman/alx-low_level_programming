f0264f4913f85c022ed6141213281c3810c243c6
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-29 20:43:39 +0000

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - Frees lists
+  * @head: Args
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	while (*head != NULL)
+	{
+		curr = *head;
+		*head = (*head)->next;
+		free(curr);
+	}
