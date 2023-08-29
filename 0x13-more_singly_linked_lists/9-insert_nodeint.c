5656534d488dd09b150bd6aeeae4d870870aa094
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-29 23:14:19 +0000

    Insert node at idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0;
+	listint_t *curr;
 
 	listint_t *new_node = malloc(sizeof(listint_t));
 
@@ -20,15 +21,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	while (*head)
+	if (idx == 0)
 	{
-		if (count = idx - 1)
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	curr = *head;
+
+	while (curr)
+	{
+		if (count == idx - 1)
 		{
-			new_node->next = (*head)->next;
-			(*head)->next = new_node;
+			new_node->next = curr->next;
+			curr->next = new_node;
 			return (new_node);
 		}
 		count++;
-		*head = (*head)->next;
+		curr = curr->next;
 	}
+
+	free(new_node);
+	return (NULL);
 }
