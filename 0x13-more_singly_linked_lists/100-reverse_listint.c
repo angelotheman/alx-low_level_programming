26e7dff50b40575fe803dd0c88513541a16eda83
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-30 22:01:27 +0000

    Reverse linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - Reverse a linked list
+  * @head: Linked List
+  *
+  * Return: The linked list
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *curr	= *head;
+	listint_t *next;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+
+		curr->next = prev;
+
+		prev = curr;
+		curr = next;
+	}
+
+	*head = prev;
+
+	return *head;
+}
