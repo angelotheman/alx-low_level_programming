2d7d61c0e50b752d2b1a6b502e6d47fc1f3d35ff
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-31 21:29:16 +0000

    Main file updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <string.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
