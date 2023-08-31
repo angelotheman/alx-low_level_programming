f2358440fb3e0612eb13f44f9745b56b631ded95
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-31 22:35:14 +0000

    Binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * binary_to_unit - Convert binary to int
+  * @b: The args
+  *
+  * Return: Converted number
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+
+	return (val);
+}
