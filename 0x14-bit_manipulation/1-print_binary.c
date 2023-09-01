91abbee4d94848618259b7a2085599ab1c36b784
Kwabena Twumasi <ktwum721@gmail.com>
2023-09-01 00:42:50 +0000

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,17 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
+	char nums;
 
-	for (i = 1 << 31; i > 0; i >>= 1)
+	nums = n & 1;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+	else
 	{
-		_putchar((n & i) ? '1' : '0');
+		print_binary(n >> 1);
+		_putchar(bits + ('0');
 	}
-
-	_putchar('\n');
 }
