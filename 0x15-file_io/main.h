5cfa4fa11db5916c8668dfb6e5d2d95d538ebe21
Kwabena Twumasi <ktwum721@gmail.com>
2023-09-05 07:47:52 +0000

    Updated header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
