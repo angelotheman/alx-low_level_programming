0fc1c198630aa8e3d0ed698a3ac035f23984759e
angelotheman <ktwum721@gmail.com>
2023-11-10 00:33:24 +0000

    Add new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,14 +15,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
-	/* Create the node */
 	new_node->n = n;
-	new_node->next = NULL;
+	new_node->next = *head;
 	new_node->prev = NULL;
 
-	/* insert at beginning */
-	new_node->next = *head;
-	(*head)->prev = new_node;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
 	*head = new_node;
 
 	return (new_node);
