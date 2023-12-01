35a558548f12279a34e890bdfcb9c692913bdda7
angelotheman <ktwum721@gmail.com>
2023-12-01 08:01:13 +0000

    Djb2 algorithm code

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Function to implement the djb2 algorithm
+ * @str: String argument
+ * 
+ * Return: Hashed function of the algorithm
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+
+	while((c == *str))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash);
+}
