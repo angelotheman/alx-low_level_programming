4de5829288a5f4f87c85c658b47b83c456973695
angelotheman <ktwum721@gmail.com>
2023-12-01 07:56:22 +0000

    Create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = calloc(table->size, sizeof(hash_node_t));
+	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (!new_hash_table->array)
 		return (NULL);
