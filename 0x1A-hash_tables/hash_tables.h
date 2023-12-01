91e8c7f9d19481775ac4b831a1f331548235662d
angelotheman <ktwum721@gmail.com>
2023-12-01 08:01:01 +0000

    updated header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,5 +38,6 @@ typedef struct hash_table_s
 
 /* Functions Used */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLE_H */
