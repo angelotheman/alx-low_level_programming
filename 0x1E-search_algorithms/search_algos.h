5f9aede7ff3f1ea724d225c80090b1658a30c92a
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-13 23:18:24 +0000

    Header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,9 @@
 
 /* MAIN FUNCTIONS */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* HELPER FUNCTIONS */
+void print_array(size_t start, size_t end);
 
 #endif /* SEARCH_ALGOS_H */
