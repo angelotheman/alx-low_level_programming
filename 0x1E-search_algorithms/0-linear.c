e95deeeb915a1a418e3fc5e33589f7ae84f07e9b
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-13 22:46:51 +0000

    Updates made to include print function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
  * linear_search - Search using the linear search algorithm
@@ -19,6 +20,8 @@ int linear_search(int *array, size_t size, int value)
 
 	for (count = 0; count < size; count++)
 	{
+
+		printf("Value checked array[%lu] = [%d]\n", count, array[count]);
 		if (array[count] == value)
 		{
 			return (count);
